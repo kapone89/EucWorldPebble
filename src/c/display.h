2fb7dfa09f523e1272587bfe00027b5ca1168faf
Lefteris @SomniusX <lefteros@me.com>
2020-04-14 09:41:03 +0300

    Update display.h

--- src/c/display.h
+++ src/c/display.h
@@ -2,6 +2,7 @@
 
 void set_angles(int *start, int *end);
 
+//void draw_display(Window **window, Layer **gui_layer, Layer **details_layer, TextLayer **text_layer_volt, TextLayer **text_layer_current, TextLayer **text_layer_time, TextLayer **text_layer_speed,
 void draw_display(Window **window, Layer **gui_layer, Layer **details_layer, TextLayer **text_layer_time, TextLayer **text_layer_speed,
 	TextLayer **text_layer_mph, TextLayer **text_layer_battery, TextLayer **text_layer_temperature,
 	BitmapLayer **battery_bitmap_layer, BitmapLayer **temperature_bitmap_layer, BitmapLayer **bt_bitmap_layer, Layer **arc_layer,
